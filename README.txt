ec9763a02a98177b000785d5b29381870ae4ebeb
Trung Trinh <trung28899@gmail.com>
2020-07-02 00:51:47 -0400

    using transition group

--- README.txt
+++ README.txt
@@ -1,4 +1,9 @@
 1. Tools used
+$ npm install react-transition-group --save
+$ npm update react react-dom
+
+- Docs: 
+https://reactcommunity.org/react-transition-group/
 
 2. Core Concepts: 
 
@@ -8,7 +13,7 @@ VER 1: Using Css Transition
 -----------------------------------------------
 Code in 2nd commit
 
-Step 1: read code in src/components/Backdrop/Backdrop.Css
+Step 1: read code in src/components/Modal/Modal.Css
 Step 2: Notice the use of Transition, Transform and opacity
 Step 3: See Modal.js, Backdrop.js and App.js to see how we 
 apply the animation
@@ -17,4 +22,14 @@ VER 2: Using Css animation (Using @keyframes)
 -----------------------------------------------
 Code in 3rd commit
 
-Step 1: see Modal.css for: @keyframes and animation property
+- see Modal.css for: @keyframes and animation property
+you will see the differences compare to previous version
+
+VER 3: Using React Transition Group
+-----------------------------------------------
+Code in 4rd commit
+
+- Video 401
+- See App.js to understand how <Transition> work. 
+See the imports and its properties
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,8 +24,66 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <br />
+        {
+          // The 'in' property if the component is shown or not
+          // There are 4 states of this Transition component:
+          /*
+            'entering'
+            'entered'
+            'exiting'
+            'exited'
+          */
+          /* timeout determine how long it takes from 'entering' to 'entered' and
+           from 'exiting' to 'exited' */
+          /*
+             mountOnEnter will mount the wrapped JSX when the state is entered
+             unmountOnExit will unmount the wrapped JSX when the state is 
+             exited so it (the red box) won't take up any space
+           */
+        }
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {
+            // (state) => <p>{state}</p>
+          }
+          {(state) => (
+            <div>
+              {/* state is the state of <Transition> which is 'entering', 'entered'
+                    'exiting', 'exited' */}
+              <p>{state}</p>
+              <div
+                style={{
+                  backgroundColor: "red",
+                  width: 100,
+                  height: 100,
+                  margin: "auto",
+                  opacity: state === "exiting" ? 0 : 1,
+                  transition: "opacity 1s ease-out",
+                }}
+              />
+            </div>
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        ) : null}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
